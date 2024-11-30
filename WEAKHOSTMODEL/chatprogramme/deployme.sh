d5d2dc1ce0e6ff5fadf770f8586e30d5b05b1275
Ben Green <ben.green@rinicom.com>
2024-11-30 17:03:40 +0000

    Wrong hostname

--- WEAKHOSTMODEL/chatprogramme/deployme.sh
+++ WEAKHOSTMODEL/chatprogramme/deployme.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-curl --remote-name  https://github.com/RinicomBG/internetapplicationsengineeringguesttalk/refs/heads/main/WEAKHOSTMODEL/chatprogramme/chatprogramme.c
+curl --remote-name https://raw.githubusercontent.com/RinicomBG/internetapplicationsengineeringguesttalk/refs/heads/main/WEAKHOSTMODEL/chatprogramme/chatprogramme.c
 gcc -o chatprogramme chatprogramme.c
