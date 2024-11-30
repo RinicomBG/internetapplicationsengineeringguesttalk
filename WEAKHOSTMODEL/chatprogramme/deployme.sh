d7cc602f3ccec4783cf8bec9b660f313fb61a86f
Ben Green <ben.green@rinicom.com>
2024-11-30 17:08:22 +0000

    Make script a little quieter

--- WEAKHOSTMODEL/chatprogramme/deployme.sh
+++ WEAKHOSTMODEL/chatprogramme/deployme.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-curl --remote-name https://raw.githubusercontent.com/RinicomBG/internetapplicationsengineeringguesttalk/refs/heads/main/WEAKHOSTMODEL/chatprogramme/chatprogramme.c
+curl --silent --remote-name https://raw.githubusercontent.com/RinicomBG/internetapplicationsengineeringguesttalk/refs/heads/main/WEAKHOSTMODEL/chatprogramme/chatprogramme.c
 gcc -o chatprogramme chatprogramme.c
